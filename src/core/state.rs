9745545072d1e70eb702ffa05708b7596e02c8c3
Lucky <zhou_2006ee@163.com>
2026-07-06 02:12:58 +0800

    feat: add global routing cursor

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,5 +1,5 @@
 use crate::{core::config::AppConfig, telemetry::store::TelemetryStore};
-use std::{path::PathBuf, sync::Arc};
+use std::{path::PathBuf, sync::{atomic::{AtomicU64, Ordering}, Arc}};
 use tokio::sync::RwLock;
 
 #[derive(Clone)]
@@ -8,4 +8,11 @@ pub struct AppState {
     pub config: Arc<RwLock<AppConfig>>,
     pub telemetry: TelemetryStore,
     pub http: reqwest::Client,
+    pub routing_cursor: Arc<AtomicU64>,
+}
+
+impl AppState {
+    pub fn next_routing_cursor(&self) -> u64 {
+        self.routing_cursor.fetch_add(1, Ordering::Relaxed)
+    }
 }
